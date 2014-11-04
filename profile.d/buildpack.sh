4158aa8cb8c8a8298c3b07481ca4b594098db8cf
Miëtek Bak <mietek@bak.io>
2014-11-04 09:54:33 +0000

    debug

--- profile.d/buildpack.sh
+++ profile.d/buildpack.sh
@@ -2,6 +2,7 @@ if ! (( ${BUILDPACK_INTERNAL_PATHS:-0} )); then
 	export BUILDPACK_INTERNAL_PATHS=1
 
 	echo '-----> Setting buildpack paths' >&2
+	echo '   *** DEBUG: .profile.d/buildpack.sh' >&2
 
 	export PATH="/app/.buildpack/bin:${PATH:-}"
 	export PATH="/app/.buildpack/lib/halcyon:${PATH}"
@@ -10,4 +11,5 @@ if ! (( ${BUILDPACK_INTERNAL_PATHS:-0} )); then
 		source <( halcyon show-paths )
 else
 	echo '-----> Buildpack paths already set' >&2
+	echo '   *** DEBUG: .profile.d/buildpack.sh' >&2
 fi
