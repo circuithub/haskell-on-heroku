e6e19b104529e932dea2fb48f860e18144a8df88
Miëtek Bak <mietek@bak.io>
2014-11-04 09:21:18 +0000

    Tweak use of paths

--- profile.d/buildpack.sh
+++ profile.d/buildpack.sh
@@ -1,7 +1,13 @@
 if ! (( ${BUILDPACK_INTERNAL_PATHS:-0} )); then
 	export BUILDPACK_INTERNAL_PATHS=1
 
-	export PATH="/app/.buildpack/bin:${PATH:-}"
+	echo '-----> Setting buildpack paths' >&2
 
-	HALCYON_NO_AUTOUPDATE=1 source <( halcyon show-paths )
+	export PATH="/app/.buildpack/bin:${PATH:-}"
+	export PATH="/app/.buildpack/lib/halcyon:${PATH}"
+
+	HALCYON_NO_AUTOUPDATE=1 \
+		source <( halcyon show-paths )
+else
+	echo '-----> Buildpack paths already set' >&2
 fi
